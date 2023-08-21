7069589a98d8a1e24a52884f9f91ec56d835669d
Rupert Dunk <rupert@rupertdunk.com>
2023-08-21 23:27:48 +0100

    fix modal background

--- src/components/BaseOverlay.vue
+++ src/components/BaseOverlay.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="relative z-30" role="dialog" aria-modal="true">
     <Transition name="tr-fade" appear @after-leave="emit('dismiss')">
-      <div v-if="active" class="fixed inset-0 bg-gray-200 dark:bg-gray-900 bg-opacity-80" />
+      <div
+        v-if="active"
+        class="fixed inset-0 bg-gray-200 bg-opacity-80 dark:bg-gray-900 dark:bg-opacity-80"
+      />
     </Transition>
     <div class="fixed inset-0 z-10 overflow-y-auto">
       <div class="flex min-h-full justify-center items-center p-4">
